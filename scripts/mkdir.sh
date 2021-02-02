e1804b10ae54de70aa3382d8da0150fcc4adf8b4
awsdrob <46461287+awsdrob@users.noreply.github.com>
2021-02-02 12:50:50 -0500

    Update mkdir.sh

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,6 @@
 mkdir -p /web
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
+# curl -O https://bootstrap.pypa.io/get-pip.py
+# python3 get-pip.py --user
+yum -y install python3
 python3 -m pip install Flask
 #sdssdsds
